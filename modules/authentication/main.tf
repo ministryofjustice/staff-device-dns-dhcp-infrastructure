f7ed71bd3d1b576c3dd0606215121d442426604a
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-07-23 13:14:53 +0100

    Added additional config for cognito pool

--- modules/authentication/main.tf
+++ modules/authentication/main.tf
@@ -4,10 +4,25 @@ resource "aws_cognito_user_pool" "pool" {
 
 resource "aws_cognito_user_pool_client" "client" {
   name = "azure-ad-client"
-  user_pool_id = "${aws_cognito_user_pool.pool.id}"
+  user_pool_id = aws_cognito_user_pool.pool.id
 }
 
 resource "aws_cognito_user_pool_domain" "main" {
   domain       = "azure-ad-authentication"
-  user_pool_id = "${aws_cognito_user_pool.pool.id}"
+  user_pool_id = aws_cognito_user_pool.pool.id
+}
+
+resource "aws_cognito_identity_provider" "cognito_identity_provider" {
+  user_pool_id  = aws_cognito_user_pool.pool.id
+  provider_name = "Azure"
+  provider_type = "SAML"
+
+  provider_details {
+    authorize_scopes       = "email,openid,aws.cognito.signin.user.admin"
+    MetaDataUrl            = var.meta_data_url
+  }
+
+  attribute_mapping {
+    email = "email"
+  }
 }
