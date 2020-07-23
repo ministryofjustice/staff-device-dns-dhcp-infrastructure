ea6779db82b17c07edb2a65b1670111965fd4a3f
Andy Mitchell <me@themitchell.co.uk>
2020-07-23 15:06:12 +0100

    Remove hello world

--- modules/hello/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "${var.prefix_name}-my-tf-test-bucket"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
--- modules/hello/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "prefix_name" {
-  type = string
-}
