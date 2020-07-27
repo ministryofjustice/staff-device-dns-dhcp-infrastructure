1ae110f058ea4bc3898ede35fd3d5167d8eb621b
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-07-27 15:33:12 +0100

    Working cognito implementation

--- outputs.tf
+++ outputs.tf
@@ -7,13 +7,18 @@ output "domain" {
 }
 
 output "sign-in-url" {
-  value = "${local.domain}.auth.${data.aws_region.current_region.id}.amazoncognito.com/login?response_type=code&client_id=${module.cognito.azure-client-id}&redirect_uri=https://localhost:4200"
+  value = "${local.domain}/login?response_type=code&client_id=${module.cognito.azure-client-id}&redirect_uri=https://localhost:80"
 }
 
 output "sign-out-url" {
-  value = "${local.domain}.auth.${data.aws_region.current_region.id}.amazoncognito.com/logout?response_type=code&client_id=${module.cognito.azure-client-id}&redirect_uri=https://localhost:4200"
+  value = "${local.domain}/logout?response_type=code&client_id=${module.cognito.azure-client-id}&redirect_uri=https://localhost:80"
 }
 
 output "reply-url" {
   value = "${local.domain}/saml2/idpresponse"
 }
+
+output "identifier-uri" {
+  value = "urn:amazon:cognito:sp:${module.cognito.cognito-pool-id}"
+}
+
