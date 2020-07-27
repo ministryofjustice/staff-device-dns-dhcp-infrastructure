f87f2ef966ec5e0d1bef033a54dd4cd5aac8a46b
Andy Mitchell <me@themitchell.co.uk>
2020-07-27 16:01:41 +0100

    Add ECS target groups to load balancer

--- modules/dhcp/main.tf
+++ modules/dhcp/main.tf
@@ -8,3 +8,31 @@ resource "aws_lb" "load_balancer" {
 
   tags = var.tags
 }
+resource "aws_lb_target_group" "target_group" {
+  name = var.prefix
+  port = 67
+  protocol = "UDP"
+  vpc_id = var.vpc_id
+  
+  health_check {
+    protocol = "TCP"
+    port = 80
+  }
+}
+
+resource "aws_lb_target_group_attachment" "target_group_attachment" {
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_instance.dhcp_server.id
+  port             = 67
+}
+
+resource "aws_lb_listener" "udp" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = "67"
+  protocol          = "UDP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
