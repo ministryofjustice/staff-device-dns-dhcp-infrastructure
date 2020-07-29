dddcdefe74b947c5a3cd3ab471eaf97453482f32
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-29 12:51:32 +0100

    Don't create parameter store for database endpoint

--- modules/dhcp/parameter_store.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ssm_parameter" "kea_server_db_hostname" {
-  name      = "/codebuild/dhcp/${var.env}/db/hostname"
-  type      = "String"
-  value     = aws_db_instance.dhcp_server_db.endpoint
-  overwrite = true
-  tags      = var.tags
-}
