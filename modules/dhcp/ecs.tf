7d4a153e98a7103e1244ed9d3866cf1bd24fdf11
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-29 15:27:12 +0100

    Allow cross-account access to ECR

--- modules/dhcp/ecs.tf
+++ modules/dhcp/ecs.tf
@@ -91,3 +91,36 @@ resource "aws_ecr_repository" "docker_dhcp_repository" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository_policy" "docker_dhcp_repository_policy" {
+  repository = aws_ecr_repository.docker_dhcp_repository.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
+            ]
+        }
+    ]
+}
+EOF
+}
