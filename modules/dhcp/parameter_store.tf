1c33135506b5437236245c5dc0dae97a5036b08c
Emile <emile.swarts@digital.justice.gov.uk>
2020-07-29 12:47:10 +0100

    Database endpoint doesn't need to be encrypted

--- modules/dhcp/parameter_store.tf
+++ modules/dhcp/parameter_store.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "kea_server_db_hostname" {
   name      = "/codebuild/dhcp/${var.env}/db/hostname"
-  type      = "SecureString"
+  type      = "String"
   value     = aws_db_instance.dhcp_server_db.endpoint
   overwrite = true
   tags      = var.tags
