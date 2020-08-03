6e124362c0c51862d9ab89571b85fae4c0db1ff9
Efua Akumanyi <efua.akumanyi@gmail.com>
2020-08-03 08:43:11 +0100

    Renamed alarm, removed extra 'dhcp'

--- modules/alarms/cloudwatch_metric_alarms.tf
+++ modules/alarms/cloudwatch_metric_alarms.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_metric_alarm" "dhcp-cluster-cpu-utilization" {
   count               = local.enabled
-  alarm_name          = "${var.prefix}-dhcp-cluster-cpu-utilization"
+  alarm_name          = "${var.prefix}-cluster-cpu-utilization"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
   metric_name         = "CPUUtilization"
