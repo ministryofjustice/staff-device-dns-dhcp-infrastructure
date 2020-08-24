a670bb127c36a0b42bd4886c5358bad6712d6df6
Emile <emile.swarts@digital.justice.gov.uk>
2020-08-24 12:30:10 +0100

    Temporarily comment out admin lb verification record

--- modules/admin/route53.tf
+++ modules/admin/route53.tf
@@ -15,23 +15,19 @@ resource "aws_route53_record" "admin_lb" {
   }
 }
 
-resource "aws_route53_record" "admin_lb_verification" {
-  # name    = aws_acm_certificate.admin_lb.domain_validation_options.resource_record_name
-  # type    = aws_acm_certificate.admin_lb.domain_validation_options.resource_record_type
-  zone_id = var.vpn_hosted_zone_id
-  # records = [aws_acm_certificate.admin_lb.domain_validation_options.resource_record_value]
-  ttl     = 60
+# resource "aws_route53_record" "admin_lb_verification" {
+#   zone_id = var.vpn_hosted_zone_id
+#   ttl     = 60
 
-  for_each = {
-    for dvo in aws_acm_certificate.admin_lb.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+#   for_each = {
+#     for dvo in aws_acm_certificate.admin_lb.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
 
-  name    = each.value.name
-  records = [each.value.record]
-  type    = each.value.type
-
-}
+#   name    = each.value.name
+#   records = [each.value.record]
+#   type    = each.value.type
+# }
