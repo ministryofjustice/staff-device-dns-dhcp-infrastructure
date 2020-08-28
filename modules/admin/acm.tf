598876d27aa305122cb8f23d1358ab0dd1382fab
Emile <emile.swarts@digital.justice.gov.uk>
2020-08-28 15:35:56 +0100

    Create custom domain for admin database

--- modules/admin/acm.tf
+++ modules/admin/acm.tf
@@ -13,3 +13,19 @@ resource "aws_acm_certificate_validation" "admin_lb" {
   certificate_arn         = aws_acm_certificate.admin_lb.arn
   validation_record_fqdns = [for record in aws_route53_record.admin_lb_verification : record.fqdn]
 }
+
+resource "aws_acm_certificate" "admin_db" {
+  domain_name       = aws_route53_record.admin_db.fqdn
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = var.tags
+}
+
+resource "aws_acm_certificate_validation" "admin_db" {
+  certificate_arn         = aws_acm_certificate.admin_db.arn
+  validation_record_fqdns = [for record in aws_route53_record.admin_db_verification : record.fqdn]
+}
