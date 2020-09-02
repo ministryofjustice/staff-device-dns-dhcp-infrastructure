96c103de4a2a56d3352ae24c1220e6ddf3148ef4
Emile <emile.swarts@digital.justice.gov.uk>
2020-09-02 11:46:31 +0100

    Remove custom domain names from admin DB

--- modules/admin/route53.tf
+++ modules/admin/route53.tf
@@ -31,37 +31,3 @@ resource "aws_route53_record" "admin_lb_verification" {
   records = [each.value.record]
   type    = each.value.type
 }
-
-resource "aws_route53_record" "admin_db" {
-  zone_id        = var.vpn_hosted_zone_id
-  name           = "staff-device-admin-db-${var.short_prefix}.${var.vpn_hosted_zone_domain}"
-  type           = "A"
-  set_identifier = var.region
-
-  alias {
-    name                   = aws_db_instance.admin_db.address
-    zone_id                = aws_db_instance.admin_db.hosted_zone_id
-    evaluate_target_health = true
-  }
-
-  latency_routing_policy {
-    region = var.region
-  }
-}
-
-resource "aws_route53_record" "admin_db_verification" {
-  zone_id = var.vpn_hosted_zone_id
-  ttl     = 60
-
-  for_each = {
-    for dvo in aws_acm_certificate.admin_db.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  name    = each.value.name
-  records = [each.value.record]
-  type    = each.value.type
-}
