c2a6fa339e59558f4024f14aa70b6be4a80091c5
Emile <emile.swarts@digital.justice.gov.uk>
2020-09-02 09:28:37 +0100

    Disable custom domain names for admin DB

--- modules/dhcp/acm.tf
+++ modules/dhcp/acm.tf
@@ -1,15 +1,15 @@
-resource "aws_acm_certificate" "dhcp_server_db" {
-  domain_name       = aws_route53_record.dhcp_server_db.fqdn
-  validation_method = "DNS"
+# resource "aws_acm_certificate" "dhcp_server_db" {
+#   domain_name       = aws_route53_record.dhcp_server_db.fqdn
+#   validation_method = "DNS"
 
-  lifecycle {
-    create_before_destroy = true
-  }
+#   lifecycle {
+#     create_before_destroy = true
+#   }
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
 
-resource "aws_acm_certificate_validation" "dhcp_server_db" {
-  certificate_arn         = aws_acm_certificate.dhcp_server_db.arn
-  validation_record_fqdns = [for record in aws_route53_record.kea_lease_db_verification : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "dhcp_server_db" {
+#   certificate_arn         = aws_acm_certificate.dhcp_server_db.arn
+#   validation_record_fqdns = [for record in aws_route53_record.kea_lease_db_verification : record.fqdn]
+# }
