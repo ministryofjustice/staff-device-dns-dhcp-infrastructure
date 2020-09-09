5cb320dad263bee7bd9cd407a3670efe99f71d1c
Emile <emile.swarts@digital.justice.gov.uk>
2020-09-09 15:22:55 +0100

    Restore Cognito Azure authentication endpoints and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+locals {
+  domain = var.enable_authentication ? "https://${module.cognito.cognito_user_pool_domain}.auth.${data.aws_region.current_region.id}.amazoncognito.com" : ""
+}
+
 output "terraform_outputs" {
   value = {
     dhcp = {
@@ -5,4 +9,20 @@ output "terraform_outputs" {
       ecr = module.dhcp.ecr
     }
   }
+}
+
+output "logoutUrl" {
+  value = var.enable_authentication ? "\"${local.domain}/logout?response_type=code&client_id=${module.cognito.cognito_user_pool_client_id}&redirect_uri=http://localhost:80\"" : ""
+}
+
+output "url" {
+  value = var.enable_authentication ? "\"${local.domain}/saml2/idpresponse\"" : ""
+}
+
+output "identifierUris" {
+  value = var.enable_authentication ? "\"urn:amazon:cognito:sp:${module.cognito.cognito_user_pool_id}\"" : ""
+}
+
+output "cognito_user_pool_id" {
+  value = var.enable_authentication ? module.cognito.cognito_user_pool_id[0] : ""
 }
