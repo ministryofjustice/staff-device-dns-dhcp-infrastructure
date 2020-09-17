0a09d36797e94afedcdd3819d060fd599cfbef3b
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-09-17 09:12:11 +0100

    Added encryption to s3 buckets

--- modules/dns_dhcp_common/s3.tf
+++ modules/dns_dhcp_common/s3.tf
@@ -5,6 +5,15 @@ resource "aws_s3_bucket" "config_bucket" {
   versioning {
     enabled = true
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.config_bucket_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
 data "template_file" "config_bucket_policy" {
@@ -20,5 +29,19 @@ resource "aws_s3_bucket_policy" "config_bucket_policy" {
   bucket = aws_s3_bucket.config_bucket.id
 
   policy = data.template_file.config_bucket_policy.rendered
+}
+
+resource "aws_s3_bucket_public_access_block" "config_bucket_public_block" {
+  bucket = aws_s3_bucket.config_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_kms_key" "config_bucket_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
 }
 
