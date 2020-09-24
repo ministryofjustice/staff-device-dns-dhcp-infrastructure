2ce06e40c8523bf32e5e09461bdc4622e2f6fec5
Emile <emile.swarts@digital.justice.gov.uk>
2020-09-24 16:45:01 +0100

    Bump health threshold to allowed minimum for health check

--- modules/dns_dhcp_common/load_balancer.tf
+++ modules/dns_dhcp_common/load_balancer.tf
@@ -35,7 +35,8 @@ resource "aws_lb_target_group" "target_group" {
     protocol = "TCP"
     port     = 80
     interval = 5
-    healthy_threshold = 1
+    healthy_threshold = 2
+    unhealthy_threshold = 2
   }
 
   depends_on = [aws_lb.load_balancer]
