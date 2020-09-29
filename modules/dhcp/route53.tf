c86a58a548b7620420874f0c8b71643ed86287e7
Emile <emile.swarts@digital.justice.gov.uk>
2020-09-29 09:46:18 +0100

    CNAME for dhcp-db does not require domain

--- modules/dhcp/route53.tf
+++ modules/dhcp/route53.tf
@@ -3,6 +3,6 @@ resource "aws_route53_record" "dhcp_db" {
   ttl     = 3600
   type    = "CNAME"
 
-  name    = "dhcp-dns-admin-dhcp-db.${var.vpn_hosted_zone_domain}"
+  name    = "dhcp-dns-admin-dhcp-db"
   records = [aws_db_instance.dhcp_server_db.address]
 }
