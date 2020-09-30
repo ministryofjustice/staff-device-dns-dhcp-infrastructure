d91c324ae63a53a247b91404f8ae77074a9e4b4a
Emile Swarts <emile.swarts123@gmail.com>
2020-09-30 11:41:14 +0100

    Output Admin ECS Service and Cluster name (#58)
    This will be referenced by the Admin Rails deploy script to do a zero
    downtime deployment of the admin service.

--- modules/admin/outputs.tf
+++ modules/admin/outputs.tf
@@ -5,3 +5,10 @@ output "admin_db_identifier" {
 output "admin_url" {
   value = aws_route53_record.admin_app.fqdn
 }
+
+output "ecs" {
+  value = {
+    cluster_name = aws_ecs_cluster.admin_cluster.name
+    service_name = aws_ecs_service.admin-service.name
+  }
+}
