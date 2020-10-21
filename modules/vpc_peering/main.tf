be47ef04832b7ae35b11ddbf94df20f537089e10
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-10-21 17:44:09 +0100

    Auto accept VPC peering

--- modules/vpc_peering/main.tf
+++ modules/vpc_peering/main.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc_peering_connection" "vpc_peering_connection" {
-  vpc_id   = var.source_vpc_id
+  vpc_id      = var.source_vpc_id
   peer_vpc_id = var.target_vpc_id
+  auto_accept = true
 }
