3f695fb156cfad76a7286e7cb78b66f201c095a7
Emile <emile.swarts@digital.justice.gov.uk>
2020-11-20 17:05:05 +0000

    Set min capacity for DHCP cluster

--- modules/dns_dhcp_common/ecs_auto_scaling.tf
+++ modules/dns_dhcp_common/ecs_auto_scaling.tf
@@ -2,7 +2,7 @@ resource "aws_appautoscaling_target" "auth_ecs_target" {
   service_namespace  = "ecs"
   resource_id        = "service/${aws_ecs_cluster.server_cluster.name}/${aws_ecs_service.service.name}"
   max_capacity       = 6
-  min_capacity       = 2
+  min_capacity       = 3
   scalable_dimension = "ecs:service:DesiredCount"
 }
 
