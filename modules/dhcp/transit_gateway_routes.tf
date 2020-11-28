47b083c5770711ad64e14d6d85153c2f6e678607
Emile <emile.swarts@digital.justice.gov.uk>
2020-11-28 16:25:06 +0000

    Reference correct variable for transit gateway id

--- modules/dhcp/transit_gateway_routes.tf
+++ modules/dhcp/transit_gateway_routes.tf
@@ -3,5 +3,5 @@ resource "aws_route" "transit-gateway" {
 
   route_table_id            = each.value
   destination_cidr_block    = "0.0.0.0/0"
-  transit_gateway_id        = var.transit_gateway_id
+  transit_gateway_id        = var.dhcp_transit_gateway_id
 }
