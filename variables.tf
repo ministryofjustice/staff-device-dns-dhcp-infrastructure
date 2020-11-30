2803e83b2d876167fe7af136f08d1c0db829776f
Neil Kidd <neilkidd@users.noreply.github.com>
2020-11-30 17:54:42 +0000

    Create BSI penetration testing VM's (#82)
    * Introduce BSI test vm module. Places a VM into each pre-production VM
    * Inject secrets for ami id and ingress cidrs

--- variables.tf
+++ variables.tf
@@ -130,6 +130,14 @@ variable "dhcp_egress_transit_gateway_routes" {
   type = set(string)
 }
 
+variable "pentesting_vm_ami_id" {
+  type = string
+}
+
+variable "pentesting_vm_ami_ingress_cidrs" {
+  type = list(string)
+}
+
 variable "corsham_vm_ip" {
   type = string
 }
