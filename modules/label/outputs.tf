28afbd69a59df25aae7078c0e9c4c4a8445f897b
Emile Swarts <emile.swarts123@gmail.com>
2020-12-04 12:27:51 +0000

    Move label definitions into module (#87)
    * Move label definitions into module
    
    There is duplication around the label definitions, consolidate this.
    Will make it easier to add labels in the future.
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ modules/label/outputs.tf
@@ -0,0 +1,11 @@
+output "tags" {
+  value = module.label.tags
+}
+
+output "id" {
+  value = module.label.id
+}
+
+output "stage" {
+  value = module.label.stage
+}
--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "is_production" {
-  type    = string
-  default = true
-}
-
-variable "owner_email" {
-  type    = string
-  default = "staff-device-dns-dhcp@digital.justice.gov.uk"
-}
-
 variable "assume_role" {
   type = string
 }
