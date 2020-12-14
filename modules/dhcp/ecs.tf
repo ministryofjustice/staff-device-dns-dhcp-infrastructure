cf5569840a295612c9384ca50b33729a2d89e46a
Emile <emile.swarts@digital.justice.gov.uk>
2020-12-14 11:34:53 +0000

    Fixup

--- modules/dhcp/ecs.tf
+++ modules/dhcp/ecs.tf
@@ -11,7 +11,7 @@ resource "aws_ecs_service" "service" {
   name            = "${var.prefix}-service"
   cluster         = aws_ecs_cluster.server_cluster.id
   task_definition = aws_ecs_task_definition.server_task.arn
-  desired_count   = "1"
+  desired_count   = 1
   launch_type     = "FARGATE"
 
   load_balancer {
