9f411b16a179440395a93c72585604fbb9b73dd5
Emile Swarts <emile.swarts123@gmail.com>
2021-01-05 13:27:38 +0000

    Restrict access to KEA API (#111)
    This allows peer heartbeats and admin API requests as the private link
    between the two VPCs goes through an NLB in the DHCP VPC

--- modules/dhcp/security_groups.tf
+++ modules/dhcp/security_groups.tf
@@ -17,13 +17,13 @@ resource "aws_security_group_rule" "dhcp_container_healthcheck" {
 }
 
 resource "aws_security_group_rule" "dhcp_container_kea_api_in" {
-  description       = "Allow TCP heartbeat in between peers"
+  description       = "Allow TCP heartbeat between peers"
   type              = "ingress"
   from_port         = 8000
   to_port           = 8000
   protocol          = "tcp"
   security_group_id = aws_security_group.dhcp_server.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = [var.vpc_cidr]
 }
 
 resource "aws_security_group_rule" "dhcp_container_kea_api_out" {
