d00637bb3bd40dc8f0dea7cc98087ad5ed0adf9a
Neil Kidd <neil.kidd@madetech.com>
2021-01-28 09:28:08 +0000

    Fix formatting

--- scripts/publish_terraform_outputs.sh
+++ scripts/publish_terraform_outputs.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 terraform_outputs=$(terraform output -json terraform_outputs)
 
 aws ssm put-parameter --name "/terraform_dns_dhcp/$ENV/outputs" \
-       --description "Terraform outputs that other pipelines or processes depend on" \
-       --value "$terraform_outputs" \
-       --type String \
-       --overwrite
+  --description "Terraform outputs that other pipelines or processes depend on" \
+  --value "$terraform_outputs" \
+  --type String \
+  --overwrite
