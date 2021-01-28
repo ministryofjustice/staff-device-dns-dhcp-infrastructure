e2441698599de41b72b1a9905364bb4c4c0cb30b
Neil Kidd <neil.kidd@madetech.com>
2021-01-28 09:20:09 +0000

    Fix  SC2006: Use  notation instead of legacy backticked

--- scripts/publish_terraform_outputs.sh
+++ scripts/publish_terraform_outputs.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-terraform_outputs=`terraform output -json terraform_outputs`
+terraform_outputs=$(terraform output -json terraform_outputs)
 
 aws ssm put-parameter --name "/terraform_dns_dhcp/$ENV/outputs" \
        --description "Terraform outputs that other pipelines or processes depend on" \
