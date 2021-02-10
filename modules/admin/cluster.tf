61def6e9046da96e8353293fa0f73677a94915b5
Joshua-Luke Bevan <joshua-luke@hotmail.co.uk>
2021-02-10 09:17:12 +0000

    fix admin naming convention

--- modules/admin/cluster.tf
+++ modules/admin/cluster.tf
@@ -13,7 +13,7 @@ resource "aws_cloudwatch_log_group" "admin_log_group" {
 }
 
 resource "aws_ecr_repository" "admin_ecr" {
-  name = "${var.prefix}-admin"
+  name = var.prefix
 
   tags = var.tags
 }
