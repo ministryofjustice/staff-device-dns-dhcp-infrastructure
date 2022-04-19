71524cd044fecadff4bce91b651f573c63401121
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-04-19 16:11:57 +0100

    :coffin: Remove redundant data block

--- modules/dns_dhcp_common/s3.tf
+++ modules/dns_dhcp_common/s3.tf
@@ -16,15 +16,6 @@ resource "aws_s3_bucket" "config_bucket" {
   }
 }
 
-# data "template_file" "config_bucket_policy" {
-#   template = file("${path.module}/policies/config_bucket_policy.json")
-
-#   vars = {
-#     config_bucket_arn = aws_s3_bucket.config_bucket.arn,
-#     ecs_task_role_arn = aws_iam_role.ecs_task_role.arn
-#   }
-# }
-
 resource "aws_s3_bucket_policy" "config_bucket_policy" {
   bucket = aws_s3_bucket.config_bucket.id
 
