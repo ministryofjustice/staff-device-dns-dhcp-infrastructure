e125601b2a06c76de0140e1d041ea7a244dab01b
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-04-20 16:53:26 +0100

    :recycle: Update aws s3 related modules

--- modules/dns_dhcp_common/s3.tf
+++ modules/dns_dhcp_common/s3.tf
@@ -1,17 +1,36 @@
 resource "aws_s3_bucket" "config_bucket" {
   bucket = "${var.prefix}-config-bucket"
-  acl    = "private"
   tags   = var.tags
-  versioning {
-    enabled = true
-  }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.config_bucket_key.arn
-        sse_algorithm     = "aws:kms"
-      }
+  lifecycle {
+    ignore_changes = [
+      server_side_encryption_configuration,
+      versioning,
+      logging,
+      acl
+    ]
+  }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.config_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.config_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.config_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.config_bucket_key.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
