b219c3a6e70342d93b2dea080d4f5ba6b5f34515
Gary H <26419401+Gary-H9@users.noreply.github.com>
2022-06-30 10:43:51 +0100

    :wrench: Increase CPU and Memory
    Increasing CPU from 512 -> 1024, this necessitates the memory be doubled as well.

--- modules/dhcp/ecs_task_definition_api.tf
+++ modules/dhcp/ecs_task_definition_api.tf
@@ -3,8 +3,8 @@ resource "aws_ecs_task_definition" "api_server_task" {
   task_role_arn            = module.dns_dhcp_common.iam.ecs_task_role_arn
   execution_role_arn       = module.dns_dhcp_common.iam.ecs_execution_role_arn
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
+  cpu                      = "1024"
+  memory                   = "2048"
   network_mode             = "awsvpc"
 
   container_definitions = <<EOF
