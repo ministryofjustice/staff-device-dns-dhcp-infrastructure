eeafd16577f3e63e8d6f830efafb82a6243058a1
Mitch Dawson <mitchell.dawson@justice.gov.uk>
2022-10-13 12:38:28 +0100

    Revert "Merge pull request #207 from ministryofjustice/mTouhid-patch-3"
    This reverts commit 52c2b3aa2221f3fa6a3d4422a4fe2e0028e58f98, reversing
    changes made to cf5a310182b6def522f2e8f3dee78587ec918774.

--- modules/dns/security_groups.tf
+++ modules/dns/security_groups.tf
@@ -26,32 +26,12 @@ resource "aws_security_group_rule" "dns_container_udp_in" {
   cidr_blocks       = [var.vpc_cidr]
 }
 
-resource "aws_security_group_rule" "dns_container_tcp_in" {
-  description       = "Allow inbound TCP traffic to the BIND server"
-  type              = "ingress"
-  from_port         = 53
-  to_port           = 53
-  protocol          = "tcp"
-  security_group_id = aws_security_group.dns_server.id
-  cidr_blocks       = [var.vpc_cidr]
-}
-
 resource "aws_security_group_rule" "dns_container_udp_out" {
   description       = "Allow outbound traffic from the BIND server"
   type              = "egress"
   from_port         = 53
   to_port           = 53
   protocol          = "udp"
-  security_group_id = aws_security_group.dns_server.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "dns_container_tcp_out" {
-  description       = "Allow outbound TCP traffic from the BIND server"
-  type              = "egress"
-  from_port         = 53
-  to_port           = 53
-  protocol          = "tcp"
   security_group_id = aws_security_group.dns_server.id
   cidr_blocks       = ["0.0.0.0/0"]
 }
