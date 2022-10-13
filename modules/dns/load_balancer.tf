eb1830f5657fcd7b8460e6e902bfa1689c57f280
Mitch Dawson <mitchell.dawson@justice.gov.uk>
2022-10-13 12:37:44 +0100

    Revert "Merge pull request #210 from ministryofjustice/lb_target_group_name"
    This reverts commit 927179124ede8217d3d1a710fd52f493511676f9, reversing
    changes made to b44b436f8a4c984c7fbf3d7b6a24465b746098dd.

--- modules/dns/load_balancer.tf
+++ modules/dns/load_balancer.tf
@@ -41,8 +41,8 @@ resource "aws_lb_listener" "tcp" {
   }
 }
 
-resource "aws_lb_target_group" "target_group_udp" {
-  name                 = "${var.prefix}-udp"
+resource "aws_lb_target_group" "target_group" {
+  name                 = var.prefix
   protocol             = "UDP"
   vpc_id               = var.vpc_id
   port                 = "53"
@@ -60,7 +60,7 @@ resource "aws_lb_target_group" "target_group_udp" {
 }
 
 resource "aws_lb_target_group" "target_group_tcp" {
-  name                 = "${var.prefix}-tcp"
+  name                 = var.prefix
   protocol             = "TCP"
   vpc_id               = var.vpc_id
   port                 = "5353"
