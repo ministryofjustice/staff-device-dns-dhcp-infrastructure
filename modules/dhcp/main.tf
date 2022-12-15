0c8bf35f6337b1a38309a3141d56988975abac6d
Mitch Dawson <mitchell.dawson@justice.gov.uk>
2022-12-15 11:32:42 +0000

    :wrench: Add shared_services_account_id variable to dhcp main.tf

--- modules/dhcp/main.tf
+++ modules/dhcp/main.tf
@@ -1,6 +1,7 @@
 module "dns_dhcp_common" {
-  source = "../dns_dhcp_common"
-  prefix = var.prefix
-  tags   = var.tags
-  vpc_id = var.vpc_id
+  source                     = "../dns_dhcp_common"
+  prefix                     = var.prefix
+  tags                       = var.tags
+  vpc_id                     = var.vpc_id
+  shared_services_account_id = var.shared_services_account_id
 }
