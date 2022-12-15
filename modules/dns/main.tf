87f6fb732c801b2cc64fda05fbfb45a47704c7a8
Mitch Dawson <mitchell.dawson@justice.gov.uk>
2022-12-15 11:43:47 +0000

    :wrench: Add var to dns module

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -8,8 +8,9 @@ terraform {
 }
 
 module "dns_dhcp_common" {
-  source = "../dns_dhcp_common"
-  prefix = var.prefix
-  vpc_id = var.vpc_id
-  tags   = var.tags
+  source                     = "../dns_dhcp_common"
+  prefix                     = var.prefix
+  vpc_id                     = var.vpc_id
+  tags                       = var.tags
+  shared_services_account_id = var.shared_services_account_id
 }
