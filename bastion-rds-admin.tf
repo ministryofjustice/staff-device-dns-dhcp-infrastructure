b1b9f9a9114b314ae1c6b147ffed2e3e74925c1f
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-11-06 19:05:32 +0000

    Amend bastion details

--- bastion-rds-admin.tf
+++ bastion-rds-admin.tf
@@ -7,8 +7,8 @@ module "rds_admin_label" {
 module "rds_admin" {
   source             = "./modules/bastion"
   prefix             = module.rds_admin_label.id
-  vpc_id             = module.servers_vpc.vpc.vpc_id
-  vpc_cidr_block     = module.servers_vpc.vpc.vpc_cidr_block
+  vpc_id             = module.admin_vpc.vpc.vpc_id
+  vpc_cidr_block     = module.admin_vpc.vpc.vpc_cidr_block
   private_subnets    = module.admin_vpc.public_subnets
   security_group_ids = [module.admin.security_group_ids.admin_ecs]
   number_of_bastions = 1
