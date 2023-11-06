0795f66974e13980a3f289693e974c8e3d37628b
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-11-06 16:52:45 +0000

    Remove bastion SG port 22 ingress/egress
    This bastion module use SSM Session Manager, post 22 isn't required, as no SSH access from the internet is allowed.

--- modules/bastion/security_groups.tf
+++ modules/bastion/security_groups.tf
@@ -3,13 +3,6 @@ resource "aws_security_group" "bastion" {
   description = "Allow SSH into bastion"
   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
