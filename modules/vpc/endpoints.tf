570d3b0ba8d7cec845318c9ecfdbe09d40ffc673
Stephen James <stephen.james4@digital.justice.gov.uk>
2023-11-10 17:14:50 +0000

    Fix endpoint route table for S3

--- modules/vpc/endpoints.tf
+++ modules/vpc/endpoints.tf
@@ -22,7 +22,7 @@ resource "aws_vpc_endpoint" "rds" {
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id          = module.vpc.vpc_id
-  route_table_ids = module.vpc.private_route_table_ids
+  route_table_ids = module.vpc.public_route_table_ids
   service_name    = "com.amazonaws.${var.region}.s3"
   tags            = var.tags
 }
