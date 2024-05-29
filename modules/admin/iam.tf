3c573768b212b8e5015cab5219b160ed13b4a8cd
Asif Amirat <asif.amirat@justice.gov.uk>
2024-05-29 16:28:49 +0100

    attaching ssm readonly iam policy in place of ssm full access iam policy as we should not be granting full access

--- modules/admin/iam.tf
+++ modules/admin/iam.tf
@@ -63,7 +63,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment" {
   for_each = toset([
     "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy", 
-    "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+    "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
   ])
 
   role       = aws_iam_role.ecs_execution_role.name
