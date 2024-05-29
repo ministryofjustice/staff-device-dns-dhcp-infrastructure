3929b4ee65c0aa52ac0b6496245c358a79f96c87
Jahir <jahir.uddin@digital.justice.gov.uk>
2024-05-29 17:14:05 +0100

    upgraded DB instance class,added new CA,enabled performnace insights for DHCP and DHCP admin DB

--- modules/admin/db.tf
+++ modules/admin/db.tf
@@ -3,28 +3,30 @@ locals {
 }
 
 resource "aws_db_instance" "admin_db" {
-  allocated_storage           = 20
-  storage_type                = "gp2"
-  engine                      = "mysql"
-  engine_version              = "8.0"
-  auto_minor_version_upgrade  = true
-  allow_major_version_upgrade = false
-  apply_immediately           = true
-  instance_class              = "db.t2.medium"
-  identifier                  = "${var.prefix}-db"
-  db_name                     = replace(var.prefix, "-", "")
-  username                    = var.admin_db_username
-  password                    = var.admin_db_password
-  backup_retention_period     = var.admin_db_backup_retention_period
-  multi_az                    = true
-  storage_encrypted           = true
-  db_subnet_group_name        = aws_db_subnet_group.admin_db_group.name
-  vpc_security_group_ids      = [aws_security_group.admin_db.id]
-  monitoring_role_arn         = aws_iam_role.rds_monitoring_role.arn
-  monitoring_interval         = 60
-  skip_final_snapshot         = true
-  deletion_protection         = local.is_production ? true : false
-  publicly_accessible         = false
+  allocated_storage            = 20
+  storage_type                 = "gp2"
+  engine                       = "mysql"
+  engine_version               = "8.0"
+  auto_minor_version_upgrade   = true
+  allow_major_version_upgrade  = false
+  apply_immediately            = true
+  instance_class               = "db.t3.medium"
+  identifier                   = "${var.prefix}-db"
+  db_name                      = replace(var.prefix, "-", "")
+  username                     = var.admin_db_username
+  password                     = var.admin_db_password
+  backup_retention_period      = var.admin_db_backup_retention_period
+  multi_az                     = true
+  storage_encrypted            = true
+  db_subnet_group_name         = aws_db_subnet_group.admin_db_group.name
+  vpc_security_group_ids       = [aws_security_group.admin_db.id]
+  monitoring_role_arn          = aws_iam_role.rds_monitoring_role.arn
+  monitoring_interval          = 60
+  skip_final_snapshot          = true
+  deletion_protection          = local.is_production ? false : false
+  publicly_accessible          = false
+  performance_insights_enabled = true
+  ca_cert_identifier           = "rds-ca-rsa2048-g1"
 
   enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
 
