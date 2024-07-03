85392f7af0985aa65f562bfe453d32e8c578eee3
James Green <james.green6@justice.gov.uk>
2024-07-03 07:21:27 +0100

    updated all secrets

--- secrets_manager.tf
+++ secrets_manager.tf
@@ -1,21 +1,23 @@
-resource "aws_secretsmanager_secret" "example" {
-  name = "/test/${var.env}/db"
+resource "aws_secretsmanager_secret" "codebuild_dhcp_env_admin_db" {
+  name = "/codebuild/dhcp/${var.env}/admin/db"
+  provider = "aws.env"
 }
 
-resource "aws_secretsmanager_secret_version" "rds_credentials" {
-  secret_id     = aws_secretsmanager_secret.example.id
+resource "aws_secretsmanager_secret_version" "codebuild_dhcp_env_admin_db" {
+  provider = "aws.env"
+  secret_id     = aws_secretsmanager_secret.codebuild_dhcp_env_admin_db.id
   secret_string = jsonencode(
     merge(
       {
         "username" : "adminuser",
-        "password" : random_password.db_password.result
+        "password" : random_password.codebuild_dhcp_env_admin_db.result
       },
       module.admin.admin_db_details
     )
   )
 }
 
-resource "random_password" "db_password"{
+resource "random_password" "codebuild_dhcp_env_admin_db"{
   length           = 24
   special          = true
   override_special = "_!%^"
@@ -28,26 +30,86 @@ resource "random_password" "db_password"{
   }
 }
 
+resource "aws_secretsmanager_secret" "codebuild_dhcp_env_db" {
+  name = "/codebuild/dhcp/${var.env}/db"
+  provider = "aws.env"
+}
 
-# resource "aws_secretsmanager_secret" "example" {
-#   name = "/codebuild/dhcp/${var.env}/db"
-# }
-#
-# resource "aws_secretsmanager_secret" "example" {
-#   name = "/staff-device/dhcp/sentry_dsn"
-# }
-#
-# resource "aws_secretsmanager_secret" "example" {
-#   name = "/staff-device/dns/sentry_dsn"
-# }
-#
-# resource "aws_secretsmanager_secret" "example" {
-#   name = "/codebuild/dhcp/${var.env}/admin/api"
-# }
-#
-# resource "aws_secretsmanager_secret" "example" {
-#   name = "/staff-device/admin/sentry_dsn"
-# }
-# resource "aws_secretsmanager_secret" "example" {
-#   name = "/codebuild/dhcp/${var.env}/admin/db"
-# }
+resource "aws_secretsmanager_secret_version" "codebuild_dhcp_env_db" {
+  provider = "aws.env"
+  secret_id     = aws_secretsmanager_secret.codebuild_dhcp_env_db.id
+  secret_string = jsonencode(
+    merge(
+      {
+        "username" : "dhcpuser",
+        "password" : random_password.codebuild_dhcp_env_db.result
+      },
+      module.dhcp.dhcp_db_details
+    )
+  )
+}
+
+resource "random_password" "codebuild_dhcp_env_db"{
+  length           = 24
+  special          = true
+  override_special = "_!%^"
+
+  lifecycle {
+    ignore_changes = [
+      length,
+      override_special
+    ]
+  }
+}
+
+resource "aws_secretsmanager_secret" "staff_device_dhcp_sentry_dsn" {
+  name = "/staff-device/dhcp/sentry_dsn"
+  provider = "aws.env"
+}
+
+resource "aws_secretsmanager_secret_version" "staff_device_dhcp_sentry_dsn" {
+  provider = "aws.env"
+  secret_id     = aws_secretsmanager_secret.staff_device_dhcp_sentry_dsn.id
+  secret_string = "REPLACE_ME"
+}
+
+resource "aws_secretsmanager_secret" "staff_device_dns_sentry_dsn_1" {
+  name = "/staff-device/dns/sentry_dsn"
+  provider = "aws.env"
+}
+
+resource "aws_secretsmanager_secret_version" "staff_device_dns_sentry_dsn" {
+  provider = "aws.env"
+  secret_id     = aws_secretsmanager_secret.staff_device_dns_sentry_dsn_1.id
+  secret_string = "REPLACE_ME"
+}
+
+resource "aws_secretsmanager_secret" "staff_device_admin_sentry_dsn_1" {
+  name = "/staff-device/admin/sentry_dsn"
+  provider = "aws.env"
+}
+
+resource "aws_secretsmanager_secret_version" "staff_device_admin_sentry_dsn" {
+  provider      = "aws.env"
+  secret_id     = aws_secretsmanager_secret.staff_device_admin_sentry_dsn_1.id
+  secret_string = "REPLACE_ME"
+}
+
+resource "aws_secretsmanager_secret" "codebuild_dhcp_env_admin_api" {
+  name = "/codebuild/dhcp/${var.env}/admin/api"
+  provider = "aws.env"
+}
+
+resource "aws_secretsmanager_secret_version" "codebuild_dhcp_env_admin_api" {
+  provider = "aws.env"
+  secret_id     = aws_secretsmanager_secret.codebuild_dhcp_env_admin_api.id
+  secret_string = jsonencode(
+    merge(
+      {
+        "endpoint" : "REPLACE_ME",
+        "basic_auth_username" : "REPLACE_ME",
+        "basic_auth_password" : "REPLACE_ME",
+      }
+    )
+  )
+}
