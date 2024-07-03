28527759db1114841a489503e9b44dc5da38c13a
James Green <james.green6@justice.gov.uk>
2024-07-03 06:04:58 +0100

    wip

--- secrets_manager.tf
+++ secrets_manager.tf
@@ -4,12 +4,15 @@ resource "aws_secretsmanager_secret" "example" {
 
 resource "aws_secretsmanager_secret_version" "rds_credentials" {
   secret_id     = aws_secretsmanager_secret.example.id
-  secret_string = merge(tomap(
-    {
-      "username" : "adminuser",
-      "password" : "${random_password.db_password.result}"
-    }
-  ), "${module.admin.admin_db_details}")
+  secret_string = jsonencode(
+    merge(
+      {
+        "username" : "adminuser",
+        "password" : random_password.db_password.result
+      },
+      module.admin.admin_db_details
+    )
+  )
 }
 
 resource "random_password" "db_password"{
