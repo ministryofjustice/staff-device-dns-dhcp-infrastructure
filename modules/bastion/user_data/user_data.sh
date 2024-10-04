e18e5c528dbabab055a66942c5ffd1a5619dfa46
Stephen.James <stephen.james4@digital.justice.gov.uk>
2024-10-04 16:56:51 +0100

    Removing integrated bastion module
    No longer required now remote module is ready to use.
    
    ND-533

--- modules/bastion/user_data/user_data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-mkdir ~/.aws
-cat << 'EOF' > ~/.aws/config
-[profile s3-role]
-role_arn = arn:aws:iam::683290208331:role/s3-mojo-file-transfer-assume-role
-credential_source = Ec2InstanceMetadata
-EOF
